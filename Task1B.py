da2280f0aecb5896ef8a14f65d043f9d1d0e6666
Alan Dong <ayd27@cam.ac.uk>
2022-01-29 21:36:22 +0000

    implented stations_by_distance in geo

---
+++ Task1B.py
@@ -0,0 +1,5 @@
+from floodsystem.geo import stations_by_distance
+
+def run():
+    """Requirements for Task 1B"""
+    pass
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,4 +6,14 @@ geographical data.
 
 """
 
+from haversine import haversine, Unit
 from .utils import sorted_by_key  # noqa
+
+def stations_by_distance(stations, p):
+    """Calculates distance of stations in km from a coordinate p"""
+    distances = []
+    for station in stations: 
+        distance = haversine(station.coord, p, unit=Unit.KILOMETERS)
+        distances.append((station, distance))
+        sorted_by_key(distances, 1)
+    return distances
